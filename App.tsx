aa3471f7d8685d40af10c3b768857ed695887ce5
Freddie <freddie@insightml.io>
2024-07-11 01:16:03 +0100

    three main containers and char_img added

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -19,8 +20,64 @@ import {
 
 function App(): React.JSX.Element {
   return (
-    <Text>Test</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.char_image_section}>
+        <View style={styles.char_image_container}>
+          <Image
+            source={require('./assets/char_image.png')}
+            style={styles.char_image}
+          />
+        </View>
+      </View>
+      <View style={styles.skills_section}>
+        <Text>Section 2</Text>
+      </View>
+      <View style={styles.info_section}>
+        <Text>Section 3</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 18,
+  },
+  char_image_section: {
+    flex: 518 / 1234,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#000', // Optional: to visualize the sections
+  },
+  skills_section: {
+    flex: 127 / 1234,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#000', // Optional: to visualize the sections
+  },
+  info_section: {
+    flex: 589 / 1234,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#000', // Optional: to visualize the sections
+  },
+  char_image_container: {
+    marginTop: 50,
+    margin: 30,
+    borderRadius: 15,
+    borderWidth: 4,
+    borderColor: 'red',
+    overflow: 'hidden', // Ensures the image fits within the rounded corners
+  },
+  char_image: {
+    flex: 1,
+    aspectRatio: 1792 / 1024,
+  },
+});
 
 export default App;
